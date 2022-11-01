d43ac977e484446def5faf93a906ff9d5372ae07
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-01 13:23:00 -0300

    Heading tests

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,16 +1,8 @@
-import { screen } from '@testing-library/react';
-
 import { renderTheme } from '../../styles/renderTheme.js';
-import { theme } from '../../styles/theme';
 import Home from '.';
 
-test('renders learn react link', () => {
-  renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: /Hello/i,
-  }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondaryColor,
+describe('<Home />', () => {
+  it('should render <Home />', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
 });
