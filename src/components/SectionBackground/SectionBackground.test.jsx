039fa577a75f9fa501808af1927678dd9f6647d7
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-02 19:18:33 -0300

    creating container and background components

---
+++ src/components/SectionBackground/SectionBackground.test.jsx
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/renderTheme';
+import { SectionBackground } from '.';
+
+describe('<SectionBackground />', () => {
+  it('should render', () => {
+    renderTheme(<SectionBackground>Children</SectionBackground>);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
