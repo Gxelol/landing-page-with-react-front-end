fe141cb9cd1a2907beda0a2ccc6a4928aba78c1a
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-03 20:45:44 -0300

    Menu, medias and css

---
+++ src/components/Menu/stories.jsx
@@ -0,0 +1,27 @@
+import { Menu } from '.';
+
+import linksMock from '../NavLink/mock';
+
+export default {
+  title: 'Menu',
+  component: Menu,
+  args: {
+    links: linksMock,
+    logoData: {
+      text: 'Logo',
+      link: '#target',
+      srcImg: '',
+    },
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Menu {...args} />
+    </div>
+  );
+};
