e5070ebfb7e0bb79003cd72650e077f973398554
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-06 18:54:42 -0300

    creating and testing component GridContent

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { Text } from '../Text';
+import * as Styled from './styles';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase light={background}>
+          {title}
+        </Heading>
+        <Styled.HtmlContainer>
+          <Text>{html}</Text>
+        </Styled.HtmlContainer>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.node.isRequired,
+  html: P.node.isRequired,
+  background: P.bool,
+};
