0d7c37cdeff3066cfbeeb2dc73c659f16d889839
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-11 18:27:11 -0300

    testing mapping with sections two Columns

---
+++ src/api/map-sections.js
@@ -0,0 +1,110 @@
+import { Grid3x3 } from '@styled-icons/material-outlined';
+
+export const mapSections = (sections = []) => {
+  return sections.map((section) => {
+    if (section.__component === 'section.section-two-columns') {
+      return mapSectionTwoColumns(section);
+    }
+
+    if (section.__component === 'section.section-content') {
+      return mapSectionContent(section);
+    }
+
+    if (section.__component === 'section.section-grid') {
+      const { __component: { text_grid = [], image_grid = [] } = '' } = section;
+
+      if (text_grid.lenght > 0) {
+        return mapGridText(section);
+      }
+
+      if (image_grid.lenght > 0) {
+        return mapGridImage(section);
+      }
+    }
+
+    return section;
+  });
+};
+
+export const mapSectionTwoColumns = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    description: text = '',
+    image: { url: srcImg = '' } = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+
+  return {
+    component,
+    title,
+    text,
+    srcImg,
+    background,
+    sectionId,
+  };
+};
+
+export const mapSectionContent = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    content: html = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+
+  return {
+    component,
+    title,
+    html,
+    background,
+    sectionId,
+  };
+};
+
+export const mapGridText = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    description = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+    text_grid: textGrid = [],
+  } = section;
+
+  return {
+    component: 'section.grid-text',
+    title,
+    description,
+    background,
+    sectionId,
+    textGrid,
+  };
+};
+
+export const mapGridImage = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    description = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+    image_grid: imageGrid = [],
+  } = section;
+
+  return {
+    component: 'section.grid-image',
+    title,
+    description,
+    background,
+    sectionId,
+    imageGrid: imageGrid.map((img) => {
+      const {
+        image: { url: srcImg = '', alternativeText: altText = '' } = '',
+      } = img;
+
+      return {
+        srcImg,
+        altText,
+      };
+    }),
+  };
+};
