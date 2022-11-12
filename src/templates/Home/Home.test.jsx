a8d842572be4774e80d930a78693e2405a727912
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-12 14:23:54 -0300

    cleaning tests

--- src/templates/Home/Home.test.jsx
+++
@@ -1,8 +0,0 @@
-import { renderTheme } from '../../styles/renderTheme.js';
-import Home from '.';
-
-describe('<Home />', () => {
-  it('should render <Home />', () => {
-    renderTheme(<Home />);
-  });
-});
