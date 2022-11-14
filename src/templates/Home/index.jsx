dca461dc4ad11d8e3b4ca754fb7e43b14803103a
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-14 16:13:16 -0300

    loading api data in front-end

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,41 @@
-import { Heading } from '../../components/Heading';
-import { Base } from '../Base';
+import { useEffect, useRef, useState } from 'react';
+
+import { mapData } from '../../api/map-data';
+
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
 import { mockBase } from '../Base/mock';
-import * as Styled from './styles';
+import { Base } from '../Base';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(
+        `http://localhost:1337/api/pages/?populate[menu][populate]=*&populate[sections][populate]=*`,
+      );
+      const json = await data.json();
+      const { attributes } = json.data[0];
+      const pageData = mapData([attributes]);
+      setData(pageData[0]);
+    };
+
+    load();
+    // return () => {};
+  }, []);
+
+  if (data === undefined) {
+    return <h1>Page not found.</h1>;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Loading...</h1>;
+  }
+
   return <Base {...mockBase} />;
 }
 
